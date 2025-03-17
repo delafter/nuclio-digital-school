97e5baf2829a5760c7bec8cf497b561a72797508
Manu <delafter@gmail.com>
2025-03-17 20:44:53 +0100

    viajanda correctamente al estar en login o joinnow

--- src/index.js
+++ src/index.js
@@ -7,16 +7,26 @@ import Login from "./components/Login/Login.jsx";
 import JoinNow from "./components/JoinNow/JoinNow.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import School from "./views/School.jsx";
+import Master from "./views/Master.jsx";
+import Faculty from "./views/Faculty.jsx";
+import Content from "./views/Content.jsx";
+import CareerTalent from "./views/CareerTalent.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <Navbar />
+    <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join-now" element={<JoinNow />} />
+        <Route path="/home" element={<School />} />
+        <Route path="/master" element={<Master />} />
+        <Route path="/faculty" element={<Faculty />} />
+        <Route path="/content" element={<Content />} />
+        <Route path="/career-talent" element={<CareerTalent />} />
       </Routes>
     </Router>
   </React.StrictMode>
